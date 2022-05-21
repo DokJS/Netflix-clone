6c0a966ec0a8ad983920433ead9d90923454204b
DokJS <omarmbengue6919@gmail.com>
2022-05-21 23:33:25 +0000

    Main component is done

--- netflix/src/Components/Main.jsx
+++ netflix/src/Components/Main.jsx
@@ -14,21 +14,29 @@ const Main = () => {
             console.log(error)
         })
     },[])
-  console.log(movies)
-  return movie && (
+  console.log(movies);
+  const truncString = (string,number)=>{
+  if(string?.length > number){
+      const stringArr = string.split('');
+      return `${stringArr.slice(0,number).join('')}...`
+  }else {
+      return string
+  }
+  }
+  return  (
     <div className='w-full h-[550px] text-white relative'>
         <div className='w-full h-[550px] z-10 absolute bg-gradient-to-r from-black'></div>
         <div className='w-full h-full'>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="movie couverture"  className='w-full h-full object-cover object-center'/>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`} alt="movie couverture"  className='w-full h-full object-cover object-center'/>
         </div>
-        <div className='absolute z-20  w-full text-white top-2/4 px-4 space-y-2 sm:space-y-3'>
-            <h1 className='font-medium text-2xl'>{movie.title}</h1>
+        <div className='absolute z-20  w-full text-white top-[20%] px-4 space-y-2 sm:space-y-3'>
+            <h1 className='font-bold text-3xl md:text-5xl'>{movie?.title}</h1>
             <div className='flex space-x-2'>
                 <button className='px-4 py-2 font-semibold bg-white rounded text-black text-xs capitalize'>Play</button>
                 <button className='bg-transparent border-gray-300 border-[1px] tracking-wider text-xs px-4 py-2 capitalize'>Watch Later</button>
             </div>
-            <span className='block text-gray-500 opacity-75'>Released {movie.release_date}</span>
-            <p className=''>{movie.overview}</p>
+            <span className='block text-gray-500 opacity-75'>Released {movie?.release_date}</span>
+            <p className=' text-gray-200 w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%]'>{truncString(movie?.overview,150)}</p>
         </div>
     </div>
   )
